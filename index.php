7c5ae716b7d04fd59d15a7e73b4601444489ab2b
Noah Mason <noah@dreamsicle.io>
2018-04-21 20:40:48 -0400

    Refactors index to hook structure.

--- index.php
+++ index.php
@@ -17,42 +17,10 @@ get_header();
 
 	<main><?php
 		
-		if ( have_posts() ) {
-
-			if ( is_home() && ! is_front_page() ) {
-				?>
-				<header>
-
-					<h1><?php 
-
-						single_post_title(); 
-
-					?></h1>
-
-				</header>
-				<?php
-			}
-
-			/* Start the Loop */
-			while ( have_posts() ) {
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/card', get_post_type() );
-
-			}
-
-			the_posts_pagination();
-
-		} else {
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		}
+		/**
+		 * Hook: WPMDC Index.
+		 */
+		do_action( 'wpmdc_index' );
 		
 	?></main>
 
