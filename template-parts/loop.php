1437fb136790e8329ecf466a222c4eb8ad1b4739
Noah Mason <noah@dreamsicle.io>
2018-04-21 20:39:26 -0400

    Adds Loop template partial.

---
+++ template-parts/loop.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * The loop template file.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package     wpmdc
+ * @subpackage  template-parts
+ */
+?>
+
+<section><?php 
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) { the_post();
+
+			/*
+			 * Include the Post-Type-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+			 */
+			get_template_part( 'template-parts/card', get_post_type() );
+
+		}
+
+		the_posts_pagination();
+
+	} else {
+
+		get_template_part( 'template-parts/content', 'none' );
+
+	}
+
+?></section>
