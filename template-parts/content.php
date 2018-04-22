54b04a756a2e47b29426fa4b5cbbdf4bbfbc96f2
Noah Mason <noah@dreamsicle.io>
2018-04-21 20:39:48 -0400

    Makes content and card templates.

---
+++ template-parts/content.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * The Singular Content.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-template-parts
+ *
+ * @package     wpmdc
+ * @subpackage  template-parts
+ */
+
+global $post;
+
+?>
+
+<article <?php wpmdc_post_id_attr() . post_class(); ?>><?php 
+
+	/**
+	 * Hook: WPMDC Card.
+	 *
+	 * @since  0.0.1 
+	 * @param  object  $post  The global post object.
+	 */
+	do_action( 'wpmdc_content', $post );
+
+?></article>
