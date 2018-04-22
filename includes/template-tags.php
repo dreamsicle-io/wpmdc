bba0b6841c6fcf13613609ada4726f3b6d51f4fc
Noah Mason <noah@dreamsicle.io>
2018-04-21 20:51:31 -0400

    Comments the template tags.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -1,5 +1,20 @@
 <?php
+/**
+ * Template Tags.
+ *
+ * @link https://codex.wordpress.org/Template_Tags
+ *
+ * @package     wpmdc
+ * @subpackage  includes
+ */
 
+/**
+ * WPMDC Get Post "id" Attribute.
+ *
+ * @since   0.0.1 
+ * @param   integer  $post_id  The ID of the post to create the attribute for.
+ * @return  string             The filtered "id" attribute value.
+ */
 function wpmdc_get_post_id_attr( $post_id = 0 ) {
 	$post = get_post( $post_id );
 	$pieces = array( $post->post_type, $post->ID );
@@ -7,6 +22,12 @@ function wpmdc_get_post_id_attr( $post_id = 0 ) {
 	return apply_filters( 'wpmdc_post_id_attr', implode( $sep, $pieces ), $pieces, $sep, $post );
 }
 
+/**
+ * WPMDC Post "id" Attribute.
+ *
+ * @since   0.0.1 
+ * @return  void
+ */
 function wpmdc_post_id_attr() {
 	printf(	' id="%1$s" ', esc_attr( wpmdc_get_post_id_attr() )	);
 }
