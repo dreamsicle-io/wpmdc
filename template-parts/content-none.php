d3ef0a6e42adab24b1cc5b2b52418116f0d3c4be
Noah Mason <noah@dreamsicle.io>
2018-04-25 02:37:12 -0400

    Prefixes all content templates with "content-".

---
+++ template-parts/content-none.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Not Found.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package     wpmdc
+ * @subpackage  template-parts
+ */
+
+?>
+
+<section id="not_found">
+
+	<h2><?php 
+	
+		echo esc_html_x( 'Nothing Found.', 'not found template title', 'wpmdc' );
+
+	?></h2>
+
+</section>
