f269a725c8053100d8a939ce290b4f37455198d6
Noah Mason <noah@dreamsicle.io>
2018-04-25 02:35:48 -0400

    Refactors masthead template.

--- template-parts/masthead.php
+++ template-parts/masthead.php
@@ -9,34 +9,35 @@
  */
 ?>
 
-<header id="masthead" class="site-header">
-	
-	<div class="site-branding">
-		<?php
-		the_custom_logo();
-		if ( is_front_page() && is_home() ) {
-			?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php
-		} else {
-			?>
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-		}
-		$wpmdc_description = get_bloginfo( 'description', 'display' );
-		if ( $wpmdc_description || is_customize_preview() ) {
-			?>
-			<p class="site-description"><?php echo $wpmdc_description; /* WPCS: xss ok. */ ?></p>
-		<?php } ?>
-	</div>
+<header id="masthead">
 
-	<nav id="site-navigation" class="main-navigation"><?php
-		
+	<?php if ( has_custom_logo() ) {
+
+		the_custom_logo(); 
+	
+	} ?>
+
+	<h1 class="site-title">
+
+		<a 
+		href="<?php echo esc_url( home_url( '/' ) ); ?>" 
+		title="<?php echo esc_attr( get_bloginfo( 'description' ) ) ?>"
+		rel="home"><?php 
+
+			bloginfo( 'name' ); 
+
+		?></a>
+
+	</h1>
+
+	<?php if ( has_nav_menu( 'menu-1' ) ) {
+
 		wp_nav_menu( array(
+			'container'      => 'nav', 
 			'theme_location' => 'menu-1',
 			'menu_id'        => 'primary-menu',
-		) );
-	
-	?></nav>
+		) ); 
+
+	} ?>
 
 </header>
