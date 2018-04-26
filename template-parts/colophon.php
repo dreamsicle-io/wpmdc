d14dd49c5a9388e0a45ffc26207178c17b4e32a9
Noah Mason <noah@dreamsicle.io>
2018-04-25 21:48:57 -0400

    Ensures all gettext functions are escaped.

--- template-parts/colophon.php
+++ template-parts/colophon.php
@@ -23,7 +23,7 @@ $theme = wp_get_theme();
 	
 		/* translators: 1: current year, 2: site title link. */
 		printf( 
-			_x( '&copy; %1$d %2$s', 'site copyright', 'wpmdc' ), 
+			esc_html_x( '&copy; %1$d %2$s', 'site copyright', 'wpmdc' ), 
 			date( 'Y' ), 
 			'<a href="' . esc_url( home_url( '/' ) ) . '">' . esc_html( get_bloginfo( 'name' ) ) . '</a>'
 		);
@@ -34,7 +34,7 @@ $theme = wp_get_theme();
 		
 		/* translators: 1: theme author link. */
 		printf( 
-			_x( 'Designed by %1$s', 'site designer', 'wpmdc' ),  
+			esc_html_x( 'Designed by %1$s', 'site designer', 'wpmdc' ),  
 			'<a href="' . esc_url( $theme->get( 'AuthorURI' ) ) . '" target="_blank" rel="noopener noreferrer">' . esc_html( $theme->get( 'Author' ) ) . '</a>'
 		);
 
