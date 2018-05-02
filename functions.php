17625c1c88c370ee90ce07f56e62b836e18ba82a
Noah Mason <noah@dreamsicle.io>
2018-05-02 15:21:23 -0400

    Imports new classes and initializes.
    * Imports and inits WPMDC_Widgets class.
    * Imports categroies widget and category walker classes.

--- functions.php
+++ functions.php
@@ -13,11 +13,14 @@ $template_directory = get_template_directory();
  * Require all files.
  */
 require $template_directory . '/includes/class-wpmdc.php';
-require $template_directory . '/includes/class-wpmdc-assets.php';
+require $template_directory . '/includes/class-wpmdc-widgets.php';
 require $template_directory . '/includes/class-wpmdc-navigation.php';
+require $template_directory . '/includes/class-wpmdc-assets.php';
 require $template_directory . '/includes/class-wpmdc-customizer.php';
 require $template_directory . '/includes/class-wpmdc-widget-nav-menu.php';
 require $template_directory . '/includes/class-wpmdc-widget-archives.php';
+require $template_directory . '/includes/class-wpmdc-widget-categories.php';
+require $template_directory . '/includes/class-wpmdc-walker-category.php';
 
 /**
  * Initialize all classes.
@@ -27,6 +30,7 @@ require $template_directory . '/includes/class-wpmdc-widget-archives.php';
  * hook to be hooked properly.
  */
 add_action( 'after_setup_theme', array( new WPMDC, 'init' ), 0 );
-add_action( 'after_setup_theme', array( new WPMDC_Assets, 'init' ), 0 );
+add_action( 'after_setup_theme', array( new WPMDC_Widgets, 'init' ), 0 );
 add_action( 'after_setup_theme', array( new WPMDC_Navigation, 'init' ), 0 );
+add_action( 'after_setup_theme', array( new WPMDC_Assets, 'init' ), 0 );
 add_action( 'after_setup_theme', array( new WPMDC_Customizer, 'init' ), 0 );
