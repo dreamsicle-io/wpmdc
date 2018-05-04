ad45f39cbb0376587a40346fefdeb80886fd2292
Noah Mason <noah@dreamsicle.io>
2018-05-04 10:46:39 -0400

    Adds the card component.

--- template-parts/content-card.php
+++ template-parts/content-card.php
@@ -17,40 +17,83 @@ global $post;
 
 $permalink = get_permalink();
 $title_attr = the_title_attribute( array( 'echo' => false ) );
+$excerpt = get_the_excerpt( $post->ID );
 
 ?>
 
-<article id="<?php echo esc_attr( $post->post_type . '_' . $post->ID ); ?>" <?php post_class(); ?>><?php 
+<article id="<?php echo esc_attr( $post->post_type . '_' . $post->ID ); ?>" <?php post_class( 'wpmdc-card mdc-card' ); ?>>
 	
-	if ( has_post_thumbnail() ) { ?>
-		
-		<a 
-		href="<?php echo esc_url( $permalink ); ?>" 
-		title="<?php echo esc_attr( $title_attr ); ?>" 
-		rel="bookmark"><?php 
-		
-			the_post_thumbnail( 'medium', array( 
-				'alt' => esc_attr( $title_attr ) 
-			) );
+	<a 
+	href="<?php echo esc_url( $permalink ); ?>" 
+	class="mdc-card__primary-action mdc-ripple-surface"
+	title="<?php echo esc_attr( $title_attr ); ?>"
+	rel="bookmark">
 
-		?></a>
+		<?php if ( has_post_thumbnail() ) { ?>
+		
+			<header 
+			class="mdc-card__media mdc-card__media--16-9" 
+			style="background-image:url('<?php echo esc_url( get_the_post_thumbnail_url( $post->ID, 'medium' ) ); ?>');"></header>
 
-	<?php }
+		<?php } ?>
+		
+		<div class="wpmdc-card__content">
+
+			<?php the_title( '<h3 class="mdc-typography--headline6">', '</h3>' ); ?>
+
+			<h4 class="mdc-typography--subtitle2"><?php 
+
+				if ( $post->post_type === 'post' ) {
+
+					printf( 
+						/* translators: 1: post author, 2: post publish date, 3: post publish time. */
+						esc_html_x( 'Posted by %1$s on %2$s at %3$s', 'post card byline', 'wpmdc' ), 
+						get_the_author(), 
+						get_the_date(), 
+						get_the_time() 
+					);
+
+				} 
+
+			?></h4>
+
+			<?php if ( $excerpt ) { 
+
+				echo apply_filters( 'the_excerpt', $excerpt );
+
+			} ?>
+
+		</div>
 	
-	the_title( '<h3><a href="' . esc_url( $permalink ) . '" rel="bookmark">', '</a></h3>' );
+	</a>
 
-	if ( $post->post_type !== 'page' ) {
+	<footer class="mdc-card__actions">
+		
+		<div class="mdc-card__action-buttons">
+			
+			<button class="mdc-button mdc-card__action mdc-card__action--button mdc-ripple-surface">
 
-		printf( 
-			/* translators: 1: post author, 2: post publish date, 3: post publish time. */
-			esc_html_x( 'Posted by %1$s on %2$s at %3$s', 'post card byline', 'wpmdc' ), 
-			get_the_author_posts_link(), 
-			get_the_date(), 
-			get_the_time() 
-		);
+				Read
 
-	}
+			</button>
+		
+		</div>
+		
+		<div class="mdc-card__action-icons">
+			
+			<i 
+			class="material-icons mdc-ripple-surface mdc-card__action mdc-card__action--icon" 
+			tabindex="0" 
+			role="button" 
+			title="More options" 
+			data-mdc-ripple-is-unbounded="true">
 
-	the_excerpt();
+				more_vert
 
-?></article>
+			</i>
+		
+		</div>
+	
+	</footer>
+
+</article>
