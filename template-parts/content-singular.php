60cf2d475d264acb8448e5b0b69c7f8b06043698
Noah Mason <noah@dreamsicle.io>
2018-05-04 19:49:45 -0400

    Wraps singular post in mdc card.

--- template-parts/content-singular.php
+++ template-parts/content-singular.php
@@ -19,42 +19,31 @@ $title_attr = the_title_attribute( array( 'echo' => false ) );
 
 ?>
 
-<article id="<?php echo esc_attr( $post->post_type . '_' . $post->ID ); ?>" <?php post_class(); ?>><?php 
+<article id="<?php echo esc_attr( $post->post_type . '_' . $post->ID ); ?>" <?php post_class( 'wpmdc-card mdc-card' ); ?>> 
 	
-	if ( has_post_thumbnail() ) { ?>
-		
-		<a 
-		href="<?php echo esc_url( get_the_post_thumbnail_url( 0, 'full' ) ); ?>" 
-		title="<?php echo esc_attr( $title_attr ); ?>" 
-		rel="bookmark"><?php 
-		
-			the_post_thumbnail( 'large', array( 
-				'alt' => esc_attr( $title_attr ) 
-			) );
+	<div class="wpmdc-card__content"><?php 
 
-		?></a>
+		the_title( '<h1 class="mdc-typography--headline3">', '</h1>' );
 
-	<?php }
-	
-	the_title( '<h1 class="wpmdc-no-margin-top">', '</h1>' );
+		if ( $post->post_type !== 'page' ) {
 
-	if ( $post->post_type !== 'page' ) {
+			printf( 
+				/* translators: 1: post author, 2: post publish date, 3: post publish time. */
+				_x( 'Posted by %1$s on %2$s at %3$s', 'singular post byline', 'wpmdc' ), 
+				get_the_author_posts_link(), 
+				get_the_date(), 
+				get_the_time() 
+			);
 
-		printf( 
-			/* translators: 1: post author, 2: post publish date, 3: post publish time. */
-			_x( 'Posted by %1$s on %2$s at %3$s', 'singular post byline', 'wpmdc' ), 
-			get_the_author_posts_link(), 
-			get_the_date(), 
-			get_the_time() 
-		);
+		}
 
-	}
+		the_content( _x( 'Read More', 'post content read more link', 'wpmdc' ) );
 
-	the_content( _x( 'Read More', 'post content read more link', 'wpmdc' ) );
+		wp_link_pages( array(
+			'before' => '<div>',
+			'after'  => '</div>',
+		) );
 
-	wp_link_pages( array(
-		'before' => '<div>',
-		'after'  => '</div>',
-	) );
+	?></div>
 
-?></article>
+</article>
