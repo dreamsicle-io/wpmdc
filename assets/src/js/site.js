e5c5611bb0df5911d1af5c4b48130b7f100dbab1
Noah Mason <noah@dreamsicle.io>
2018-05-07 17:21:11 -0400

    Imports and inits mdc text field js.

--- assets/src/js/site.js
+++ assets/src/js/site.js
@@ -1,6 +1,7 @@
 import { wpmdcMobileAdminBarFix } from './modules/admin-bar';
 import { wpmdcTopAppBars } from './modules/top-app-bar';
 import { wpmdcTemporaryDrawers } from './modules/drawer';
+import { wpmdcTextFields } from './modules/text-field';
 import { wpmdcSelects } from './modules/select';
 import { wpmdcRipples } from './modules/ripple';
 
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	wpmdcMobileAdminBarFix();
 	wpmdcTopAppBars();
 	wpmdcTemporaryDrawers();
+	wpmdcTextFields();
 	wpmdcSelects();
 	wpmdcRipples();
 });
