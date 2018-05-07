18c2f0329a0953c6ea1da42cdec53fd7b3179688
Noah Mason <noah@dreamsicle.io>
2018-05-07 17:18:53 -0400

    Moves active sidebar check to top of footer widgets template.

--- template-parts/footer-widgets.php
+++ template-parts/footer-widgets.php
@@ -8,24 +8,24 @@
  * @subpackage  template-parts
  */
 
-if ( is_active_sidebar( 'footer' ) ) { ?>
+if ( ! is_active_sidebar( 'footer' ) ) {
+	return;
+} ?>
 
-	<section id="footer_widgets" class="wpmdc-section mdc-theme--surface mdc-theme--on-surface mdc-elevation--z4">
+<aside id="footer_widgets" class="wpmdc-section mdc-theme--surface mdc-theme--on-surface mdc-elevation--z4">
 
-		<?php if ( is_active_sidebar( 'footer' ) ) { ?>
+	<?php if ( is_active_sidebar( 'footer' ) ) { ?>
 
-			<div class="mdc-layout-grid wpmdc-contain-desktop">
+		<div class="mdc-layout-grid wpmdc-contain-desktop">
 
-				<div class="mdc-layout-grid__inner widget-area"><?php 
+			<div class="mdc-layout-grid__inner widget-area"><?php 
 
-					dynamic_sidebar( 'footer' );
+				dynamic_sidebar( 'footer' );
 
-				?></div>
+			?></div>
 
-			</div>
+		</div>
 
-		<?php } ?>
+	<?php } ?>
 
-	</section>
-
-<?php }
+</aside>
