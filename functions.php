367e30436e76c8cd380ce02e1f11228124144d66
Noah Mason <noah@dreamsicle.io>
2018-05-31 18:12:11 -0400

    Defines path constants.

--- functions.php
+++ functions.php
@@ -7,34 +7,41 @@
  * @package wpmdc
  */
 
-$template_directory = get_template_directory();
+/**
+ * Define path constants.
+ */
+define( 'WPMDCINC', get_template_directory() . '/includes' );
 
 /**
  * Require all files.
  */
-require $template_directory . '/includes/class-wpmdc.php';
-require $template_directory . '/includes/class-wpmdc-widgets.php';
-require $template_directory . '/includes/class-wpmdc-navigation.php';
-require $template_directory . '/includes/class-wpmdc-comments.php';
-require $template_directory . '/includes/class-wpmdc-assets.php';
-require $template_directory . '/includes/class-wpmdc-customizer.php';
-require $template_directory . '/includes/class-wpmdc-component.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-nav-menu.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-pages.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-posts.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-comments.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-archives.php';
-require $template_directory . '/includes/widgets/class-wpmdc-widget-terms.php';
-require $template_directory . '/includes/walkers/class-wpmdc-walker-page.php';
-require $template_directory . '/includes/walkers/class-wpmdc-walker-category.php';
-require $template_directory . '/includes/components/class-wpmdc-layout-grid.php';
-require $template_directory . '/includes/components/class-wpmdc-text-field.php';
-require $template_directory . '/includes/components/class-wpmdc-checkbox.php';
-require $template_directory . '/includes/components/class-wpmdc-switch.php';
-require $template_directory . '/includes/components/class-wpmdc-button.php';
-require $template_directory . '/includes/tags/component.php';
-require $template_directory . '/includes/tags/top-app-bar.php';
-require $template_directory . '/includes/tags/hero.php';
+require WPMDCINC . '/class-wpmdc.php';
+require WPMDCINC . '/class-wpmdc-options.php';
+require WPMDCINC . '/class-wpmdc-widgets.php';
+require WPMDCINC . '/class-wpmdc-navigation.php';
+require WPMDCINC . '/class-wpmdc-comments.php';
+require WPMDCINC . '/class-wpmdc-assets.php';
+require WPMDCINC . '/class-wpmdc-customizer.php';
+require WPMDCINC . '/class-wpmdc-component.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-nav-menu.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-pages.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-posts.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-comments.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-archives.php';
+require WPMDCINC . '/widgets/class-wpmdc-widget-terms.php';
+require WPMDCINC . '/walkers/class-wpmdc-walker-page.php';
+require WPMDCINC . '/walkers/class-wpmdc-walker-category.php';
+require WPMDCINC . '/components/class-wpmdc-layout-grid.php';
+require WPMDCINC . '/components/class-wpmdc-text-field.php';
+require WPMDCINC . '/components/class-wpmdc-checkbox.php';
+require WPMDCINC . '/components/class-wpmdc-switch.php';
+require WPMDCINC . '/components/class-wpmdc-radio.php';
+// require WPMDCINC . '/components/class-wpmdc-icon-button.php';
+require WPMDCINC . '/components/class-wpmdc-icon-toggle.php';
+require WPMDCINC . '/components/class-wpmdc-button.php';
+require WPMDCINC . '/tags/component.php';
+require WPMDCINC . '/tags/top-app-bar.php';
+require WPMDCINC . '/tags/hero.php';
 
 /**
  * Initialize all classes.
@@ -44,6 +51,7 @@ require $template_directory . '/includes/tags/hero.php';
  * hook to be hooked properly.
  */
 add_action( 'after_setup_theme', array( new WPMDC, 'init' ), 0 );
+add_action( 'after_setup_theme', array( new WPMDC_Options, 'init' ), 0 );
 add_action( 'after_setup_theme', array( new WPMDC_Widgets, 'init' ), 0 );
 add_action( 'after_setup_theme', array( new WPMDC_Navigation, 'init' ), 0 );
 add_action( 'after_setup_theme', array( new WPMDC_Comments, 'init' ), 0 );
