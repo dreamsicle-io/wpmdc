065293f4b4a432b6421b5039ac95cece196593c9
Noah Mason <noah@dreamsicle.io>
2018-06-01 18:18:42 -0400

    Removes auto option scripts from icon button and icon toggle.

--- assets/src/js/modules/icon-button.js
+++ assets/src/js/modules/icon-button.js
@@ -1,19 +1,8 @@
 import { MDCIconButton } from '@material/icon-button/dist/mdc.iconButton';
 
-function wpmdcHandleIconButtonUpdate(optionName, isOn) {
-	console.log(optionName, isOn);
-}
-
 export function wpmdcIconButton(element) {
 	if (element) {
 		MDCIconButton.attachTo(element);
-		const optionName = element.getAttribute('data-wpmdc-option-name');
-		if (optionName) {
-			element.addEventListener('MDCIconButton:change', (e) => {
-				const { isOn } = e.detail;
-				wpmdcHandleIconButtonUpdate(optionName, isOn);
-			});
-		}
 	}
 }
 
--- assets/src/js/modules/icon-toggle.js
+++ assets/src/js/modules/icon-toggle.js
@@ -1,19 +1,8 @@
 import { MDCIconToggle } from '@material/icon-toggle/dist/mdc.iconToggle';
 
-function wpmdcHandleIconToggleUpdate(optionName, isOn) {
-	console.log(optionName, isOn);
-}
-
 export function wpmdcIconToggle(element) {
 	if (element) {
 		MDCIconToggle.attachTo(element);
-		const optionName = element.getAttribute('data-wpmdc-option-name');
-		if (optionName) {
-			element.addEventListener('MDCIconToggle:change', (e) => {
-				const { isOn } = e.detail;
-				wpmdcHandleIconToggleUpdate(optionName, isOn);
-			});
-		}
 	}
 }
 
