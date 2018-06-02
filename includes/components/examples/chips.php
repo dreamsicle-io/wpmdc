6737b0d6850fd8004218388ad8b85a9ae041d849
Noah Mason <noah@dreamsicle.io>
2018-06-02 04:14:01 -0400

    Adds chip examples.

--- includes/components/examples/chips.php
+++ includes/components/examples/chips.php
@@ -47,6 +47,77 @@ if ( ! defined( 'ABSPATH' ) ) {
 				) ) );
 
 			WPMDC_Layout_Grid::close_cell();
+			WPMDC_Layout_Grid::open_cell( array( 'desktop' => 12, 'tablet'  => 8, 'phone'  => 4 ) ); 
+				
+				WPMDC_Chip::open_set();
+					wpmdc_component( new WPMDC_Chip() );
+					wpmdc_component( new WPMDC_Chip() );
+					wpmdc_component( new WPMDC_Chip() );
+					wpmdc_component( new WPMDC_Chip( array( 'disabled' => true ) ) );
+				WPMDC_Chip::close_set();
+
+				WPMDC_Chip::open_set( array( 'mod' => 'choice' ) );
+					wpmdc_component( new WPMDC_Chip( array( 'selected' => true ) ) );
+					wpmdc_component( new WPMDC_Chip() );
+					wpmdc_component( new WPMDC_Chip() );
+					wpmdc_component( new WPMDC_Chip( array( 'disabled' => true ) ) );
+				WPMDC_Chip::close_set();
+
+				WPMDC_Chip::open_set( array( 'mod' => 'filter' ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'      => 'filter',
+						'selected' => true,  
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 'mod' => 'filter' ) ) );
+					wpmdc_component( new WPMDC_Chip( array( 'mod' => 'filter' ) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'      => 'filter', 
+						'disabled' => true, 
+					) ) );
+				WPMDC_Chip::close_set();
+
+				WPMDC_Chip::open_set( array( 'mod' => 'filter' ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'      => 'filter', 
+						'icon'     => 'face', 
+						'selected' => true, 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'  => 'filter', 
+						'icon' => 'face', 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'  => 'filter', 
+						'icon' => 'face', 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'mod'      => 'filter', 
+						'icon'     => 'face', 
+						'disabled' => true, 
+					) ) );
+				WPMDC_Chip::close_set();
+
+				WPMDC_Chip::open_set();
+					wpmdc_component( new WPMDC_Chip( array( 
+						'icon'      => 'face', 
+						'removable' => true, 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'icon'      => 'face', 
+						'removable' => true, 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'icon'      => 'face', 
+						'removable' => true, 
+					) ) );
+					wpmdc_component( new WPMDC_Chip( array( 
+						'icon'      => 'face', 
+						'removable' => true, 
+						'disabled'  => true, 
+					) ) );
+				WPMDC_Chip::close_set();
+
+			WPMDC_Layout_Grid::close_cell();
 		WPMDC_Layout_grid::close_inner();
 	WPMDC_Layout_grid::close_grid(); ?>
 
